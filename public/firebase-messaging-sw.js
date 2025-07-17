e8cf61b9faf32ee3ba6407b41d264d1d8e40e679
Venghak2 <sroeunvenghak@gmail.com>
2025-07-17 16:57:52 +0700

    rm double initializeApp

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -62,17 +62,17 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 // Initialize Firebase app (use your Firebase config)
-const firebaseConfig   = {
-  apiKey: "AIzaSyDEknhMLtR7yGyjfjI3Reyn1WGvkL9K6aI",
-  authDomain: "notification-91d47.firebaseapp.com",
-  projectId: "notification-91d47",
-  storageBucket: "notification-91d47.firebasestorage.app",
-  messagingSenderId: "931826122946",
-  appId: "1:931826122946:web:7840927810f854470ebb5b",
-  measurementId: "G-30JJFRDNSX"
-};
+// const firebaseConfig   = {
+//   apiKey: "AIzaSyDEknhMLtR7yGyjfjI3Reyn1WGvkL9K6aI",
+//   authDomain: "notification-91d47.firebaseapp.com",
+//   projectId: "notification-91d47",
+//   storageBucket: "notification-91d47.firebasestorage.app",
+//   messagingSenderId: "931826122946",
+//   appId: "1:931826122946:web:7840927810f854470ebb5b",
+//   measurementId: "G-30JJFRDNSX"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // Handle background messages
